9e4f5b61582a5eaa6954b23da309ed50a955e73f
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-19 09:00:23 +0600

    Initial Commit

---
+++ CustomToastDemo/CustomToastDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToastDemoApp.swift
+//  CustomToastDemo
+//
+//  Created by Joynal Abedin on 19/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomToastDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
